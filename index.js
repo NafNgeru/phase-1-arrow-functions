fa95939f23ec6f11e8d638b61709472992dd2bbf
Naftaly Ngeru <n.ngeru.k@gmail.com>
2024-12-04 12:37:15 +0300

    Completed Lab

---
+++ index.js
@@ -0,0 +1,19 @@
+let x = 2000;
+let y = 100;
+function divide(a, b){
+    return a / b;
+}
+console.log(divide(x, y));
+
+
+const square = (x) => {
+    return x * x;
+}
+console.log(square(4));
+
+let a = 3
+let b = 4
+const add = (a, b) => {
+    return a + b;
+}
+console.log(add(a, b));
